7349ec0b0313e0974032652f735ff241553135df
Sergey Besedin <kr3ssh@gmail.com>
2014-08-09 19:55:43 +0400

    Enforce https everywhere

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Zumhotface
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.middleware.use Rack::SslEnforcer, ignore: '/assets', except_environments: 'development', except_agents: /[Googlebot|bingbot]/
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
