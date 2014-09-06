7fb766d997be1b575078bf4d9b2468f8f672cb91
Sergey Besedin <kr3ssh@gmail.com>
2014-09-07 02:23:01 +0400

    fix Key name conflict

--- app/models/key/gpg.rb
+++ app/models/key/gpg.rb
@@ -1,19 +1,17 @@
-module Key
-  class GPG < Key
-    ENCRYPTION_TYPES = %w(AES AES192 AES256 CAMELLIA128 CAMELLIA192 CAMELLIA256)
-    SIZE = 1024..4096
-    DEFAULT_ENCRYPTION_TYPE = 'AES256'
-    DEFAULT_SIZE = 2048
+class Key::GPG < Key
+  ENCRYPTION_TYPES = %w(AES AES192 AES256 CAMELLIA128 CAMELLIA192 CAMELLIA256)
+  SIZE = 1024..4096
+  DEFAULT_ENCRYPTION_TYPE = 'AES256'
+  DEFAULT_SIZE = 2048
 
-    def self.provider
-      'GPG'
-    end
+  def self.provider
+    'GPG'
+  end
 
-    def generate_key
-      pubkey RSA
-      Cipher AES
-      puts "gpg -q --gen-key -#{encryption_type} -passout pass:#{passphrase} #{numbits}"
-      --passphrase
-    end
+  def generate_key
+    # pubkey RSA
+    # Cipher AES
+    # puts "gpg -q --gen-key -#{encryption_type} -passout pass:#{passphrase} #{numbits}"
+    # --passphrase
   end
 end
--- lib/jobs/language_detector.rb
+++ lib/jobs/language_detector.rb
@@ -1,12 +1,13 @@
 require 'linguist'
 
-class Jobs::LanguageDetector
-  @queue = :language
+module Jobs
+  class LanguageDetector
+    @queue = :language
 
-  def self.perform(sid)
-    @upload = Upload.find_by_sid!(sid)
-    language = Linguist::FileBlob.new(@upload.file.path).language.try(:name)
-    @upload.update_attributes(lang: language) if language
+    def self.perform(sid)
+      @upload = Upload.find_by_sid!(sid)
+      language = Linguist::FileBlob.new(@upload.file.path).language.try(:name)
+      @upload.update_attributes(lang: language) if language
+    end
   end
-
 end
