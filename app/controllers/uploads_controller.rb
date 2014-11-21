eefbe9a56929bbaba661e0b7fd7b3b94c50aa3e3
Sergey Besedin <kr3ssh@gmail.com>
2014-11-22 01:23:30 +0300

    permanent redirect for links

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -11,7 +11,7 @@ class UploadsController < ApplicationController
 
   def show
     @upload = Upload.find_by_sid!(params[:sid])
-    upload.link? && redirect_to(upload.link) && return
+    redirect_to(upload.link, status: 301) and return if upload.link?
     respond_with upload, gon: { rabl: { as: :upload }}
   end
 
