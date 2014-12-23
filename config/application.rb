1c98adce513c19610767361749007c724f504394
Sergey Besedin <kr3ssh@gmail.com>
2014-12-23 10:19:35 +0300

    fix deprecations

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Zumhotface
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('app', 'api')
 
