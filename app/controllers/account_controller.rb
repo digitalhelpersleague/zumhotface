c8e152cded3b0d347aa8fda0d273d6e519a0cc80
Sergey Besedin <kr3ssh@gmail.com>
2015-01-09 19:25:11 +0300

    refactor account controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,22 +1,20 @@
 class AccountController < ApplicationController
   before_action :authenticate_user!
+  respond_to :html, :json
 
   def index
   end
 
   def update
-    if permitted_params
-      current_user.update_with_password(permitted_params)
-      if current_user.errors.any?
-        flash[:error] = current_user.errors.messages.map { |k, v| "#{t(k)} #{v.join(', ')}" }.join('<br /> ')
-      end
-    end
-    redirect_to action: :index
+    current_user.update_with_password(permitted_params)
+    respond_with current_user, location: -> { account_path }
   end
 
   private
 
   def permitted_params
-    params.require(:user).permit(:password, :password_confirmation, :current_password)
+    params.require(:user).permit(
+      :password, :password_confirmation, :current_password
+    )
   end
 end
