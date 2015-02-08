6d9c6c0391747bf1c9a17daa5cd0d68fcce0aab3
Sergey Besedin <kr3ssh@gmail.com>
2015-02-08 22:55:47 +0300

    seed users only if ENV variable set that way

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-User.create!(
-  email: Settings.admin.email,
-  password: Settings.admin.password,
-  password_confirmation: Settings.admin.password
-).confirm!
+if ENV['ZHF_CREATE_USER'] == 'true'
+  User.create!(
+    email: Settings.admin.email,
+    password: Settings.admin.password,
+    password_confirmation: Settings.admin.password
+  ).confirm!
+end
